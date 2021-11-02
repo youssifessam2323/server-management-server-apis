5c756fd5efcb48266b2d1a1ed4eb16f17fced8ea
youssifessam2323 <yousfessaam2323@gmail.com>
2021-11-02 03:11:56 +0200

    init project

---
+++ src/main/java/io/joework/servermanagementserverapis/ServerManagementServerApisApplication.java
@@ -0,0 +1,13 @@
+package io.joework.servermanagementserverapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagementServerApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagementServerApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/joework/servermanagementserverapis/ServerManagementServerApisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.joework.servermanagementserverapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementServerApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
